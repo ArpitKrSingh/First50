070a24bb1160f1eb975f8f614f01d32c8945489c
karpit <coronanegative@hotmail.com>
2025-07-10 12:32:47 -0700

    Added more

---
+++ Programs/MultiplicationTable.py
@@ -0,0 +1,7 @@
+# Print a multiplication table for a given number.
+
+num = int(input("Enter the number you want a multiplication table of  : "))
+
+for x in range(1,11):
+    print(f"{num} x {x} = {num*x}")
+
