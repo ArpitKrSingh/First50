4f9247ec5bd616de95719392250ca36e3ce07806
karpit <coronanegative@hotmail.com>
2025-07-22 19:12:01 -0700

    Added some programs

--- Programs/PrimeNumber.py
+++ Programs/PrimeNumber.py
@@ -1,18 +1,19 @@
 # Check if a number is prime
 
-num = int(input("Enter the num you want to check prime number : "))
- # 1 3 5 7 13 17
-
-# if num % 2 == 0 :
-#     print("It is not a prime number ")
-# else:
-#     print("It is a prime number ")
+num = int(input("Enter the number you want to check if it is prime or not : "))
 
 if num < 2:
-    print("It is not a prime number ")
+    print("It is a prime number. ")
 elif num == 2:
-    print("It is a prime number")
-
-
-
+    print("It is a prime number. ")
+else:
+    is_Prime = True
+    for i in range(2, int(num ** 0.5)+1):
+        if num % i == 0:
+            is_Prime =  False
+            break
+    if is_Prime:
+        print("It is a prime number. ")
+    else:
+        print("It is not a prime number. ")
 
---
+++ Programs/ReverseNumber.py
@@ -0,0 +1,9 @@
+# Reverse a given number (e.g., 123 → 321).
+
+num = int(input("Enter the number you want the reverse of : "))
+
+num = str(num)
+
+ans = num[::-1]
+
+print(ans)
